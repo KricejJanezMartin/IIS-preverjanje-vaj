304e893998e04362aa5253b24c334b14e258b92d
KricejJanezMartin <jkricej@gmail.com>
2023-05-04 16:53:16 +0200

    evidently drift report

--- src/data/process_data.py
+++ src/data/process_data.py
@@ -6,9 +6,28 @@ df_excel = pd.read_excel('../../data/raw/students_school.xlsx', index_col=0)
 
 df_txt = pd.read_csv('../../data/raw/students.txt', delimiter='\;')
 
+"""
 print("CSV : -----")
 print(df_csv.head())
 print("EXCEL : -----")
 print(df_excel.head())
 print("TXT : -----")
-print(df_txt.head())
+print(df_txt.head())"""
+
+merged_df = pd.merge(df_csv, df_excel, on='student_id', how='outer')
+merged_df = pd.merge(merged_df, df_txt, on='student_id', how='outer')
+
+print(merged_df.head())
+
+merged_df['final_grade'] = merged_df[['G1', 'G2', 'G3']].mean(axis=1)
+
+print(merged_df.head())
+
+processed_df = merged_df.drop(['G1', 'G2', 'G3'],axis=1)
+
+print(processed_df.head())
+
+processed_df.to_csv('../../data/processed/current_data.csv', index=False)
+
+
+
